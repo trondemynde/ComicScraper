532f4ba2020843f6358b392c89f463126def065e
Ronan Sööt <112799468+trondemynde@users.noreply.github.com>
2024-05-22 10:10:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function scrapeComics() {
+async function scrapeComic(url) {
     try {
-        const response = await axios.get('https://www.buttersafe.com/');
-
+        const response = await axios.get(url);
         const $ = cheerio.load(response.data);
 
-        const comicsData = [];
-
-        // Extract the comic image from the element with id 'comic'
         const comicImage = $('#comic img').attr('src');
         const comicTitle = $('#comic img').attr('alt');
+        const prevLink = $('a[rel="prev"]').attr('href');
 
-        if (comicImage && comicTitle) {
-            comicsData.push({ title: comicTitle, image: comicImage });
-        }
-
-        console.log(comicsData);
-
+        return { title: comicTitle, image: comicImage, prevLink };
     } catch (error) {
-        console.error('Error fetching comics:', error);
+        console.error('Error scraping comic:', error);
     }
 }
 
-scrapeComics();
+async function scrapePreviousComics(startUrl, count) {
+    const comicsData = [];
+    let currentUrl = startUrl;
+
+    for (let i = 0; i < count; i++) {
+        const comic = await scrapeComic(currentUrl);
+        if (comic) {
+            comicsData.push({ title: comic.title, image: comic.image });
+            currentUrl = comic.prevLink;
+        } else {
+            break;
+        }
+    }
+
+    console.log(comicsData);
+}
+
+const startUrl = 'https://www.buttersafe.com/'; // URL of the latest comic page
+scrapePreviousComics(startUrl, 10);
